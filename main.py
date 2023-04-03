496da5dc26e01bbbf8b0d7b0370f98c29d0d34cb
Gyh-sprout <129086608+Gyh-sprout@users.noreply.github.com>
2023-04-03 14:19:04 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+result = count = countF = totalOpen = totalSubmit = 0
+srt = True
+while srt:
+    choice1 = int(input("작업을 선택하세요.\n1. 입력\n2. 계산\n"))
+
+
+    def inp():
+        global count, countF, result, totalOpen, totalSubmit
+        mul = int(input("학점을 입력하세요:\n"))
+        grade = str(input("평점을 입력하세요:\n"))
+        match grade:
+            case 'A+':
+                grade = 45
+            case 'A':
+                grade = 40
+            case 'B+':
+                grade = 35
+            case 'B':
+                grade = 30
+            case 'C+':
+                grade = 25
+            case 'C':
+                grade = 20
+            case 'D+':
+                grade = 15
+            case 'D':
+                grade = 10
+            case 'F':
+                grade = 0
+                countF += 1
+        result = result + mul * grade
+        totalOpen += mul
+        if grade == 0:
+            pass
+        else:
+            totalSubmit += mul
+        count += 1
+
+
+    def cal():
+        global srt
+        float(result)
+        print("제출용: %d학점 (GPA: %.2f)\n" % (totalSubmit, (result / 10) / (count - countF)))
+        print("열람용: %d학점 (GPA: %.2f)\n" % (totalOpen, (result / 10) / count))
+        print("\n프로그램을 종료합니다.\n")
+        srt = False
+
+
+    if choice1 == 1:
+        inp()
+    elif choice1 == 2:
+        cal()
+    else:
+        srt = False
